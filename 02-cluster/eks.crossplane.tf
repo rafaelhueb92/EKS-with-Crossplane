e17b8a0d330705858dbd7fd1a3b2d95e6584c39a
Rafael Hueb <rafaelhueb@192.168.1.6>
2025-11-30 16:09:47 -0300

    solve manifests problem

--- 02-cluster/eks.crossplane.tf
+++ 02-cluster/eks.crossplane.tf
@@ -7,25 +7,10 @@ resource "helm_release" "crossplane" {
   create_namespace = true
 
   set {
-      name  = "clusterName"
-      value = aws_eks_cluster.this.id
-  }
-
-  set {
       name  = "serviceAccount.create"
       value = true
   }
-
-  set {
-      name  = "region"
-      value = var.auth.region
-  }
   
-  set {
-      name  = "vpcId"
-      value = data.aws_vpc.this.id
-  }
-
   set{
       name  = "serviceAccount.name"
       value = "crossplane"
